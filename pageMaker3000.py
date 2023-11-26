3f323e25b0704cdba825dea88c81c9405e4d3140
Jimmy3u <jpp.olv@gmail.com>
2023-11-26 19:52:34 -0300

    feat: adicionando templates e alterações no gerador de paginas

--- pageMaker3000.py
+++ pageMaker3000.py
@@ -1,19 +1,33 @@
 import markdown
 import json
-
-with open('posts.json') as json_file:
-    dados = json.load(json_file)
-# Abre o arquivo markdown
-def parse_file(file):
+from jinja2 import Template
+# Gera o HTML do arquivo markdown
+def parse_markdown_file(file):
     md = markdown.Markdown(extensions=['fenced_code'])
     file_stream = open(file, "r")
     file_content = file_stream.read()
+    file_stream.close()
     html = md.convert(file_content)
     return html
 
+#Le os posts do arquivo json a serem convertidos
+with open('docs/posts.json') as json_file:
+    dados = json.load(json_file)
+#Le o template do post
+with open('docs/templates/post.html') as post_template:
+    post_template = Template(post_template.read())
+# Cria os posts individuais
 for post in dados:
-    print(post["titulo"])
-    print(post["autor"])
-    print(parse_file(post["arquivo"]))
+    html = post_template.render(
+        title=post["titulo"],
+        autor=post["titulo"],
+        data=post["titulo"],
+        markdown=parse_markdown_file(f'docs/posts/{post["content"]}')
+    )
+    print(html)
 
-    
+# Cria a pagina com os posts
+with open('docs/templates/blog.html') as blog_template:
+    blog_template = Template(blog_template.read())
+
+print(blog_template.render(posts=dados))
