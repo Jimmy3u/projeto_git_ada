6062e7ec9187e67b8e7ff4f558d1b0f7e40c510f
Jimmy3u <jpp.olv@gmail.com>
2023-11-26 21:55:31 -0300

    feat: atualizando templates

--- pageMaker3000.py
+++ pageMaker3000.py
@@ -1,6 +1,7 @@
 import markdown
 import json
 from jinja2 import Template
+
 # Gera o HTML do arquivo markdown
 def parse_markdown_file(file):
     md = markdown.Markdown(extensions=['fenced_code'])
@@ -10,24 +11,32 @@ def parse_markdown_file(file):
     html = md.convert(file_content)
     return html
 
+#Salva o arquivo html
+def save_html(path ,filename, content):
+    output_file = open(f'{path}/{filename}.html', "w")
+    output_file.write(content)
+    output_file.close()
+
 #Le os posts do arquivo json a serem convertidos
 with open('docs/posts.json') as json_file:
     dados = json.load(json_file)
+
 #Le o template do post
 with open('docs/templates/post.html') as post_template:
     post_template = Template(post_template.read())
+
 # Cria os posts individuais
 for post in dados:
     html = post_template.render(
         title=post["titulo"],
-        autor=post["titulo"],
-        data=post["titulo"],
+        autor=post["autor"],
+        data=post["data"],
         markdown=parse_markdown_file(f'docs/posts/{post["content"]}')
     )
-    print(html)
+    save_html("src/posts",post["slug"], html)
 
-# Cria a pagina com os posts
+# Le o template da pagina com posts e cria ela
 with open('docs/templates/blog.html') as blog_template:
     blog_template = Template(blog_template.read())
+    save_html("src/posts", "index", blog_template.render(posts=dados))
 
-print(blog_template.render(posts=dados))
